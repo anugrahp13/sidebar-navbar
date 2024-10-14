162ab5a3450e93c685bc66d8067e15d49ca3fbd6
anugrahp13 <anugrahprastyo13@gmail.com>
2024-10-15 00:24:08 +0700

    feat[App.jsx]: added function isCollapsed and handleSidebarToggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header";
 
 function App() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleSidebarToggle = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <div className="flex">
-      <Sidebar />
-      <div className="p-4 w-full">
-        <h1 className="text-3xl">Content Area</h1>
-        {/* Konten utama aplikasi di sini */}
+      <Sidebar isCollapsed={isCollapsed} />
+      <div className="w-full">
+        <Header onSidebarToggle={handleSidebarToggle} />
       </div>
     </div>
   );
