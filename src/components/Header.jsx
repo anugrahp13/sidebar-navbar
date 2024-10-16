dad73a0f7fcb6a84c091c7d1f3397e2730c6961c
anugrahp13 <anugrahprastyo13@gmail.com>
2024-10-16 20:53:50 +0700

    feat[Header.jsx]: Added icon Bell

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { DarkModeToggle } from "./elements/DarkMode";
-import { FaCog, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { FaBell, FaCog, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const Header = ({ onSidebarToggle }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,12 +11,20 @@ export const Header = ({ onSidebarToggle }) => {
     setIsMenuOpen(!isMenuOpen);
   };
   return (
-    <div className="relative flex justify-between items-center bg-white dark:bg-dark dark:text-white p-2">
+    <div className="relative flex justify-between items-center shadow-md bg-white dark:bg-dark dark:text-white py-3 px-2">
       <HiMenuAlt2
         className="w-7 h-7 cursor-pointer"
         onClick={onSidebarToggle} // Memanggil fungsi yang diberikan melalui props
       />
-      <div className="relative flex items-center gap-2">
+      <div className="relative flex items-center gap-3">
+        <div className="relative">
+          <FaBell className="h-8 w-8 lg:h-6 lg:w-6 scale-[.60] lg:scale-90" />
+          <span className="absolute top-3 -right-1 w-[1.1rem] h-[1.1rem] bg-red-500 rounded-full">
+            <span className="absolute top-[0.09rem] right-[0.30rem] text-xs font-semibold text-white">
+              2
+            </span>
+          </span>
+        </div>
         <img
           src="../../public/image/avatar/avatar.png"
           alt=""
@@ -25,25 +34,21 @@ export const Header = ({ onSidebarToggle }) => {
         <DarkModeToggle />
         {/* Dropdown menu */}
         {isMenuOpen && (
-          <div className="absolute right-0 top-12 w-40 text-sm font-semibold bg-white dark:bg-dark shadow-lg rounded-lg overflow-hidden transition-all duration-700 ease-in-out z-10">
+          <div className="absolute right-0 top-12 w-40 text-sm font-semibold bg-white dark:bg-dark shadow-md rounded-md overflow-hidden transition-all duration-700 ease-in-out z-10">
             <ul className="flex flex-col gap-2 p-2">
-              <li
-                className="flex items-center gap-2 p-2 rounded-lg text-gray-500 dark:text-white hover:text-white hover:bg-primary dark:hover:bg-primary cursor-pointer"
-                onClick={() => alert("Profile clicked")}>
+              <li className="flex items-center gap-2 p-2 text-gray-500 dark:text-link hover:dark:text-white hover:text-white hover:bg-gray-500 hover:rounded-md cursor-pointer">
                 <FaUser />
-                <span>Profile</span>
+                <Link to={"/profile"}>Profile</Link>
               </li>
-              <li
-                className="flex items-center gap-2 p-2 rounded-lg text-gray-500 dark:text-white hover:text-white hover:bg-primary dark:hover:bg-primary cursor-pointer"
-                onClick={() => alert("Settings clicked")}>
+              <li className="flex items-center gap-2 p-2 text-gray-500 dark:text-link hover:dark:text-white hover:text-white hover:bg-gray-500 hover:rounded-md cursor-pointer">
                 <FaCog />
-                <span>Settings</span>
+                <Link to={"/profile-settings"}>Settings</Link>
               </li>
             </ul>
             <hr />
             <div className="p-2">
               <div
-                className="flex items-center gap-2 p-2 rounded-lg text-gray-500 dark:text-white hover:text-white hover:bg-primary dark:hover:bg-primary cursor-pointer"
+                className="flex items-center gap-2 p-2 text-red-600 dark:text-link hover:dark:text-white hover:text-white hover:bg-red-600 hover:rounded-md cursor-pointer"
                 onClick={() => alert("Logout clicked")}>
                 <FaSignOutAlt />
                 <span>Logout</span>
