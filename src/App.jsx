edf9038aad52fde7e5144104d8c6eda3ace8da34
anugrahp13 <anugrahprastyo13@gmail.com>
2024-10-16 20:51:13 +0700

    feat[App.jsx]: Added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
-import { Sidebar } from "./components/Sidebar";
-import { Header } from "./components/Header";
+import { Layout } from "./layouts/Layout";
+import { Route, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard/Dashboard";
+import { Sales } from "./pages/sales/Sales";
+import { Items } from "./pages/managements/Items";
+import { Finance } from "./pages/finance/Finance";
+import { SalesReport } from "./pages/salesreport/SalesReport";
+import { PurchaseReport } from "./pages/purchasereport/PurchaseReport";
+import { Profile } from "./pages/profile/Profile";
+import { ProfileSettings } from "./pages/profile/ProfileSettings";
 
 function App() {
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const handleSidebarToggle = () => {
-    setIsCollapsed(!isCollapsed);
-  };
-
   return (
-    <div className="flex">
-      <Sidebar isCollapsed={isCollapsed} toggleCollapse={handleSidebarToggle} />
-      <div className="w-full">
-        <Header onSidebarToggle={handleSidebarToggle} />
-      </div>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/sales" element={<Sales />} />
+        <Route path="/finance" element={<Finance />} />
+        <Route path="/sales-report" element={<SalesReport />} />
+        <Route path="/purchase-report" element={<PurchaseReport />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile-settings" element={<ProfileSettings />} />
+      </Routes>
+    </Layout>
   );
 }
 
