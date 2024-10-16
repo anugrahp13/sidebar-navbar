e36c20d1b22f026cba40c1542bc7a217973ecaa4
anugrahp13 <anugrahprastyo13@gmail.com>
2024-10-16 20:50:20 +0700

    feat[Footer.jsx]: Added file footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <>
+      <footer className="flex justify-between items-center shadow-md bg-white dark:bg-dark py-4 px-2">
+        <p className="font-medium text-sm text-link dark:text-white">
+          Copyright © 2024 - Developed by
+          <span className="font-bold text-primary"> Anugrah Prastyo</span>. All
+          rights reserved.
+        </p>
+        <p className="font-medium text-sm text-link dark:text-white">
+          <span className="font-bold">Version</span> 1.1.0
+        </p>
+      </footer>
+    </>
+  );
+};
