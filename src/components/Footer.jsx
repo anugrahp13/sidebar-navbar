2654adfe00cd38e8c7cc6780e735fc361adce5dc
anugrahp13 <anugrahprastyo13@gmail.com>
2024-10-22 14:10:06 +0700

    feat[Footer]: Modifed background dark to gray-900

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <>
-      <footer className="flex justify-between items-center shadow-md bg-white dark:bg-dark py-4 px-2">
+      <footer className="flex justify-between items-center shadow-md bg-white dark:bg-gray-900 py-4 px-2">
         <p className="font-medium text-sm text-link dark:text-white">
           Copyright © 2024 - Developed by
           <span className="font-bold text-primary"> Anugrah Prastyo</span>. All
