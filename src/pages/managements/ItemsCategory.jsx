c544269f512f53f1a149c38e74c2548d481afea0
anugrahp13 <anugrahprastyo13@gmail.com>
2024-10-22 14:22:36 +0700

    feat[ItemsCategory.jsx]: Added file ItemsCategory

---
+++ src/pages/managements/ItemsCategory.jsx
@@ -0,0 +1,11 @@
+export const ItemsCategory = () => {
+  return (
+    <>
+      <div className="container mx-auto p-6">
+        <div className="flex justify-center items-center text-2xl">
+          Management / Items Category
+        </div>
+      </div>
+    </>
+  );
+};
