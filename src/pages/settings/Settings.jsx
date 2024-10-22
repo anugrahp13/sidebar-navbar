61187996f0f1727fa6dd5d9da0252b4e5a165822
anugrahp13 <anugrahprastyo13@gmail.com>
2024-10-22 14:22:58 +0700

    feat[Settings.jsx]: Added file Settings

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,11 @@
+export const Settings = () => {
+  return (
+    <>
+      <div className="container mx-auto p-6">
+        <div className="flex justify-center items-center text-2xl">
+          Settings
+        </div>
+      </div>
+    </>
+  );
+};
