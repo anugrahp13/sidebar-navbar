1961c557cb1b0482f10579bd4861a188aeca9e08
anugrahp13 <anugrahprastyo13@gmail.com>
2024-10-22 14:11:33 +0700

    feat[Layout.jsx]: Changed const isSidebarCollapsed to isSidebarCollapsed

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -5,20 +5,17 @@ import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
 
 export const Layout = ({ children }) => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isSidebarCollapsed, setSidebarCollapsed] = useState(false);
 
-  const handleSidebarToggle = () => {
-    setIsCollapsed(!isCollapsed);
+  const toggleSidebar = () => {
+    setSidebarCollapsed(!isSidebarCollapsed);
   };
   return (
     <>
       <div className="flex">
-        <Sidebar
-          isCollapsed={isCollapsed}
-          toggleCollapse={handleSidebarToggle}
-        />
-        <div className="w-full justify-between">
-          <Header onSidebarToggle={handleSidebarToggle} />
+        <Sidebar isCollapsed={isSidebarCollapsed} />
+        <div className="flex flex-col w-full justify-between">
+          <Header onToggleSidebar={toggleSidebar} />
           {children}
           <Outlet />
           <Footer />
